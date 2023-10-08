4c788c69e2f72ea234017303f1cab1725efa5b3d
KAsare1 <asarek977@gmail.com>
2023-10-08 22:13:47 +0000

    commit2\3

--- spms_portal/settings.py
+++ spms_portal/settings.py
@@ -77,15 +77,21 @@ WSGI_APPLICATION = 'spms_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'SPMS',
+# 		'USER': 'root',
+# 		'PASSWORD': '',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'SPMS',
-		'USER': 'root',
-		'PASSWORD': '',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
